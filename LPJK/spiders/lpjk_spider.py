b47e09a8de99cac54bd07fc099bfe101895520b1
wahyu sejati roso <wahyusejatiroso@yahoo.co..id>
2016-08-28 20:27:44 +0800

    add loader and fix error in page array

--- LPJK/spiders/lpjk_spider.py
+++ LPJK/spiders/lpjk_spider.py
@@ -3,6 +3,7 @@ import scrapy
 from scrapy import FormRequest
 from LPJK.items import LpjkItem
 from scrapy.shell import inspect_response
+from scrapy.loader import ItemLoader
 
 class LpjkSpider(scrapy.Spider):
     name = "lpjk"
@@ -13,40 +14,41 @@ class LpjkSpider(scrapy.Spider):
 
     def parse(self, response):
         propinsi = response.xpath('//select[@name="propinsi"]//@value').extract()
-        # for i in propinsi:
-        yield FormRequest.from_response(
-            response,
-            formxpath = '//form[@name="demo1"]',
-            formdata = {
-                'propinsi': "01",
-            },
-            callback = self.parse_page,
-            dont_filter = True
-        )
+        for i in propinsi:
+            yield FormRequest.from_response(
+                response,
+                formxpath = '//form[@name="demo1"]',
+                formdata = {
+                    'propinsi': i,
+                },
+                callback = self.parse_table,
+                dont_filter = True
+            )
 
     def parse_page(self, response):
-        pages = response.xpath('//select[@name="page"]/@value').extract()
-        # for p in pages:
+        pages = response.xpath('//select[@name="page"]/option/text()').extract()
+        for p in pages:
         yield FormRequest.from_response(
             response,
             formxpath = '//form[@action="status-proses-registrasi-badan-usaha-kbli-lpjk"]',
             formdata = {
-                'page': "2",
+                'page': p,
             },
             callback = self.parse_table,
             dont_filter = True
         )
            
     def parse_table(self, response):
-        item = LpjkItem()
-        item['num'] = response.xpath('//blockquote/table[@class="text"]//tr/td[1]//text()').extract()
-        item['npwp'] = response.xpath('//blockquote/table[@class="text"]//tr/td[2]//text()').extract()
-        item['tgl_permohonan'] = response.xpath('//blockquote/table[@class="text"]//tr/td[3]//text()').extract()
-        item['tgl_diterima'] = response.xpath('//blockquote/table[@class="text"]//tr/td[4]//text()').extract()
-        item['name'] = response.xpath('//blockquote/table[@class="text"]//tr/td[5]//text()').extract()
-        item['proses'] = response.xpath('//blockquote/table[@class="text"]//tr/td[6]//text()').extract()
-        item['status'] = response.xpath('//blockquote/table[@class="text"]//tr/td[7]//text()').extract()
-        yield item
+        l = ItemLoader(item = LpjkItem(), response = response)
+        l.add_xpath('num', '//blockquote/table[@class="text"]//tr/td[1]//text()')
+        l.add_xpath('npwp', '//blockquote/table[@class="text"]//tr/td[2]//text()')
+        l.add_xpath('tgl_permohonan', '//blockquote/table[@class="text"]//tr/td[3]//text()')
+        l.add_xpath('tgl_diterima', '//blockquote/table[@class="text"]//tr/td[4]//text()')
+        l.add_xpath('name', '//blockquote/table[@class="text"]//tr/td[5]//text()')
+        l.add_xpath('proses', '//blockquote/table[@class="text"]//tr/td[6]//text()')
+        l.add_xpath('status', '//blockquote/table[@class="text"]//tr/td[7]//text()')
+        print(l.load_item())
+        return l.load_item()
 
     def parse2(self, response):
         inspect_response(response, self)
